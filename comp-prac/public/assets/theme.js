7297b02c7867604109f117eee1904101d5078040
JSI <suin8@jbnu.ac.kr>
2022-03-24 01:40:55 +0900

    특정 컴포넌트 지역적, 전역적 테마 바꾸기

--- comp-prac/public/assets/theme.js
+++ comp-prac/public/assets/theme.js
@@ -17,6 +17,40 @@ const customTheme = createTheme({
       main: dangerColor,
     },
   },
+  components: {
+    MuiFab: {
+      styleOverrides: {
+        // root수준에서 바꾸면 모든 항목이 바뀐다. 조심히 사용
+        // 모든 Fab button이 바뀜
+        root: {
+          backgroundColor: "red",
+        },
+        // font-size가 small인 것에 대해 바뀜
+        sizeSmall: {
+          backgroundColor: "red",
+        },
+        // color:가 secondary로 지정된 것에 대해 바뀜
+        secondary: {
+          backgroundColor: "red",
+
+          // 마우스를 올렸을 때 바뀌는 색을 white로 지정
+          "&:hover": {
+            color: "white",
+          },
+        },
+      },
+    },
+
+    // AppBar 모든 component에 대해 danger 배경색, marginbottom 50px를 추가한다.
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          background: dangerColor,
+          marginBottom: 50,
+        },
+      },
+    },
+  },
 });
 
 export default customTheme;
