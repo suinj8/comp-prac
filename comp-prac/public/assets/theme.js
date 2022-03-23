fbbd7f7e155bac2b01fecf0e5bd23b2dd061f3d0
JSI <suin8@jbnu.ac.kr>
2022-03-24 01:15:36 +0900

    mui 테마지정

---
+++ comp-prac/public/assets/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+import { purple } from "@mui/material/colors";
+
+const primaryColor = purple[600];
+const secondaryColor = purple[50];
+const dangerColor = purple[100];
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+  },
+});
