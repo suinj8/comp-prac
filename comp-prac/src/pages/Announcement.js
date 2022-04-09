57dca41bf9a430bc61f7919fbca67183d6a5c88c
JSI <suin8@jbnu.ac.kr>
2022-04-09 19:10:02 +0900

    공지사항 page 구현3

--- comp-prac/src/pages/Announcement.js
+++ comp-prac/src/pages/Announcement.js
@@ -2,20 +2,20 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import SubHeader from "../Components/SubHeader";
 
-import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 
 const dummyRows = [
-  { id: 1, title: "컴", date: "220404" },
-  { id: 2, title: "퓨", date: "220405" },
-  { id: 3, title: "터", date: "220406" },
-  { id: 4, title: "공", date: "220407" },
-  { id: 5, title: "학", date: "220408" },
-  { id: 6, title: "부", date: "220409" },
-  { id: 7, title: "공", date: "220410" },
-  { id: 8, title: "지", date: "220411" },
-  { id: 9, title: "사항", date: "220412" },
-  { id: 10, title: "입니", date: "220413" },
-  { id: 11, title: "다", date: "220414" },
+  { id: 1, title: "컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴컴", date: "220404" },
+  { id: 2, title: "퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨퓨", date: "220405" },
+  { id: 3, title: "터터터터터터터터터터터터터터터터터터", date: "220406" },
+  { id: 4, title: "공공공공공공공공공공공공공공공공공공", date: "220407" },
+  { id: 5, title: "학학학학학학학학학학학학학학학학학학", date: "220408" },
+  { id: 6, title: "부부부부부부부부부부부부부부부부부부", date: "220409" },
+  { id: 7, title: "공공공공공공공공공공공공공공공공공공", date: "220410" },
+  { id: 8, title: "지지지지지지지지지지지지지지지지지지", date: "220411" },
+  { id: 9, title: "사항사항사항사항사항사항사항사항사항", date: "220412" },
+  { id: 10, title: "입니입니입니입니입니입니입니입니입니", date: "220413" },
+  { id: 11, title: "다다다다다다다다다다다다다다다다다다", date: "220414" },
 ];
 
 const dummycolumns: GridColDef[] = [
@@ -27,6 +27,7 @@ const dummycolumns: GridColDef[] = [
     headerAlign: "center",
     sortable: false,
     align: "center",
+    maxWidth: 100,
   },
   {
     field: "title",
@@ -44,6 +45,7 @@ const dummycolumns: GridColDef[] = [
     headerAlign: "center",
     disableColumnMenu: true,
     align: "center",
+    maxWidth: 100,
   },
 ];
 
@@ -58,9 +60,9 @@ const Announcement = () => {
           width: "85%",
           paddingTop: "3%",
           paddingBottom: "3%",
-          marginLeft: "7.5%",
-          marginRight: "7.5%",
-          justifyContent: "center",
+          paddingLeft: "7.5%",
+          paddingRight: "7.5%",
+
           display: "flex",
         }}
       >
@@ -68,7 +70,7 @@ const Announcement = () => {
           rows={dummyRows}
           columns={dummycolumns}
           pageSize={10}
-          rowsPerPageOptions={[4]}
+          rowsPerPageOptions={[3]}
           sx={{ justifyContent: "center" }}
           disableColumnSelector
           onRowClick={(params) => alert(`${params.id}번째 cell 선택됨`)}
